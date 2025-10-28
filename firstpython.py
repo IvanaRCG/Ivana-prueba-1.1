f06511d8c27614dba94a6b13c76ab5a7017ad66a
IvanaRCG <IDGallina@teco.com.ar>
2025-10-28 15:24:43 -0300

    Create firstpython.py

---
+++ firstpython.py
@@ -0,0 +1,18 @@
+Vuelta atrás
+•	Se requiere definir cómo se reincorporan las gestiones realizadas en contingencia al CRM una vez restablecido el sistema.
+•	Este punto será validado con Fiol y el equipo de provisión.
+
+
+Datos sensibles
+•	La información técnica de los SIM (IMSI, KI, ICCID) será gestionada manualmente, bajo control de responsables designados.
+•	No se automatizará ni se expondrá en bases abiertas por razones de seguridad.
+
+
+Consideraciones:
+•	Javier Hirschbein liderará el desarrollo de comandos.
+•	Walter Fiol debe validar el impacto en la cadena de provisión.
+•	Ariel López coordinará la integración con Codi y trazabilidad.
+•	Fernando Tarifa remarca que la contingencia no debe convertirse en proceso formal; debe mantenerse como excepción.
+
+Importante: si o si generar reunión con Walter FIOL ya que se requiere definir cómo se reincorporan las gestiones realizadas en contingencia al CRM una vez restablecido el sistema. Mas allá de la mediación que suele solicitarse desde el área de Hugo a Sistemas.
+
